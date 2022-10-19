6451ce51253ad09e04492d3199b22ba5cddcda4a
adrianmuresan3 <115097812+adrianmuresan3@users.noreply.github.com>
2022-10-19 21:51:23 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,15 +3,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Sammy’s makes it fun in the sun");
-        System.out.println("Introduceti perioada de inchiriere:");
+        JOptionPane.showMessageDialog(null, "Carly’s makes the food that makes it a party");
+        System.out.println("Introduceti cati invitati vor fi:");
         Scanner sc = new Scanner(System.in);
-        int hours = sc.nextInt();
-        System.out.println("Introduceti timpul extra:");
-        int time = sc.nextInt();
-        int price_per_hour = 40;
-        int price_per_minute = 1;
-        int total = (hours * price_per_hour) + (time * price_per_minute);
-        System.out.println("Ore: " + hours + ", minute: " + time + ",pret total: " + total);
+        int guests = sc.nextInt();
+        int prices = 35;
+        double eventprice;
+        eventprice = 35 * guests;
+        System.out.println("Pretul per invitat este:" + prices);
+        System.out.println("Pretul total este:" + eventprice);
+        if (guests >= 50) {
+            System.out.println("True");
+        } else {
+            System.out.println("Fals");
+        }
     }
 }
